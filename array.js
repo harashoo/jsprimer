d20842327666d899d32b68d89739fd3721e4b9b4
shogo-hr <harada.tosho.2016@gmail.com>
2019-07-27 01:27:40 +0900

    今日はここまで

--- array.js
+++ array.js
@@ -1,14 +1,28 @@
-const emptyArray = [];
-const numbers = [1, 2, 3];
+// const emptyArray = [];
+// const numbers = [1, 2, 3];
 
-const matrix = [
-  ["a", "b"],
-  ["c", "d"]
-];
+// const matrix = [
+//   ["a", "b"],
+//   ["c", "d"]
+// ];
 
-console.log(matrix[0][0]);
+// console.log(matrix[0][0]);
 
+// const array = ["one", "two", "three"];
+// console.log(array[0]);
+
+// console.log(array[array.length - 1]);
+
+// const obj = {};
+// const array = [];
+// console.log(Array.isArray(obj));
+// console.log(Array.isArray(array));
+
+// console.log(typeof array);
 const array = ["one", "two", "three"];
-console.log(array[0]);
+const [first, second, third] = array;
+console.log(first);
+console.log(second);
+console.log(third);
 
-console.log(array[array.length - 1]);
+// 配列から要素を検索から
