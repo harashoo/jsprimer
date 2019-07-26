7375bbcfb1a888f4ad5b154352228dc9b2798dc3
shogo-hr <harada.tosho.2016@gmail.com>
2019-07-27 01:19:50 +0900

    arrayやっている

---
+++ array.js
@@ -0,0 +1,14 @@
+const emptyArray = [];
+const numbers = [1, 2, 3];
+
+const matrix = [
+  ["a", "b"],
+  ["c", "d"]
+];
+
+console.log(matrix[0][0]);
+
+const array = ["one", "two", "three"];
+console.log(array[0]);
+
+console.log(array[array.length - 1]);
