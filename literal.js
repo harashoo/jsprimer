89377375eb23e9749e8bcfe66e82293331dcfde9
shogo-hr <harada.tosho.2016@gmail.com>
2019-07-30 20:32:38 +0900

    文字列終わり

--- literal.js
+++ literal.js
@@ -11,9 +11,20 @@
 // const searchPattern = /\d{3}/;
 // console.log(str.search(searchPattern));
 
-const str = "ABC あいう DE えお";
-const alphabetsPattern = /[a-zA-Z]+/g;
-let matches;
-while (matches = alphabetsPattern.exec(str)) {
-  console.log(`match: ${matches[0]}, lastIndex: ${alphabetsPattern.lastIndex}`);
+// const str = "ABC あいう DE えお";
+// const alphabetsPattern = /[a-zA-Z]+/g;
+// let matches;
+// while (matches = alphabetsPattern.exec(str)) {
+//   console.log(`match: ${matches[0]}, lastIndex: ${alphabetsPattern.lastIndex}`);
+// }
+
+function escapeURL(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+      return result + encodeURIComponent(values[i - 1]) + str;
+  });  
 }
+
+const input = "A&B";
+// escapeURLタグ関数を使ったタグ付きテンプレート
+const escapedURL = escapeURL`https://example.com/search?q=${input}&sort=desc`;
+console.log(escapedURL); // => "https://example.com/search?q=A%26B&sort=desc"
