96a3e2a6425f44d7c8450920f8b227d2486c3bc4
shogo-hr <harada.tosho.2016@gmail.com>
2019-08-08 00:25:25 +0900

    dateおわり

---
+++ date.js
@@ -0,0 +1,14 @@
+// const now = new Date();
+// console.log(Date.now());
+// console.log(now.getTime());
+// console.log(now.toISOString());
+
+// function formatDate(date) {
+//   const yyyy = new String(date.getFullYear());
+//   const mm = new String(date.getMonth() + 1).padStart(2, "0");
+//   const dd = new String(date.getDate()).padStart(2, "0");
+//   return `${yyyy}/${mm}/${dd}`;
+// }
+
+// const date = new Date("2006-01-02T15:04:05.999");
+// console.log(formatDate(date));
