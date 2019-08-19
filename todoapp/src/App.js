1781e0e584cbb5c59bfea0b0c27f21729458ddc0
shogo-hr <harada.tosho.2016@gmail.com>
2019-08-19 18:42:38 +0900

    todoappおわり

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,19 +1,49 @@
-import { element } from "./view/html-util.js";
+import { render } from "./view/html-util.js";
+import { TodoListModel } from "./model/TodoListModel.js";
+import { TodoItemModel } from "./model/TodoItemModel.js";
+import { TodoListView } from "./view/TodoListView.js";
 
 export class App {
+  constructor() {
+    this.todoListView = new TodoListView();
+    this.todoListModel = new TodoListModel();
+  }
+
+  handleAdd(title) {
+    this.todoListModel.addTodo(new TodoItemModel({ title, completed: false }));
+  }
+
+  handleUpdate({ id, completed }) {
+    this.todoListModel.updateTodo({ id, completed });
+  }
+
+  handleDelete({ id }) {
+    this.todoListModel.deleteTodo({ id });
+  }
+
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
     const containerElement = document.querySelector("#js-todo-list");
     const todoItemCountElement = document.querySelector("#js-todo-count");
 
-    let todoItemCount = 0;
+    this.todoListModel.onChange(() => {
+      const todoItems = this.todoListModel.getTodoItems();
+      const todoListElement = this.todoListView.createElement(todoItems, {
+        onUpdateTodo: ({ id, completed }) => {
+          this.handleUpdate({ id, completed });
+        },
+        onDeleteTodo: ({ id }) => {
+          this.handleDelete({ id });
+        }
+      });
+      render(todoListElement, containerElement);
+      todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
+    });
+
     formElement.addEventListener("submit", (event) => {
       event.preventDefault();
-      const todoItemElement = element`<li>${inputElement.value}</li>`;
-      containerElement.appendChild(todoItemElement);
-      todoItemCount += 1;
-      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      this.handleAdd(inputElement.value);
       inputElement.value = "";
     });
   }
